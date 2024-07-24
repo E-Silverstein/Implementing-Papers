913f5872802854a4ae7e1a0082cb33f729b9671c
Ethan Silverstein <ethanlsilverstein@gmail.com>
2024-07-24 12:19:26 -0400

    Implemented Lenet5

---
+++ LeNet.py
@@ -0,0 +1,116 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+import numpy as np
+
+# Hyperparms
+batch_size = 64
+num_classes = 10
+lr = 0.001
+num_epochs = 10
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+# Loading data
+train_data = torchvision.datasets.MNIST(
+    root="./data",
+    train=True,
+    transform=transforms.Compose(
+        [
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.013,), std=(0.30,)),
+        ]
+    ),
+    download=True,
+)
+
+test_data = torchvision.datasets.MNIST(
+    root="./data",
+    train=False,
+    transform=transforms.Compose(
+        [
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.013,), std=(0.30,)),
+        ]
+    ),
+    download=True,
+)
+
+train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
+
+class LeNet5(nn.Module):
+    def __init__(self, num_classes,*args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.c1 = nn.Sequential(
+            nn.Conv2d(1,6, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(6),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.c2 = nn.Sequential(
+            nn.Conv2d(6,16, kernel_size=5, stride=1, padding=0),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.fc = nn.Linear(400, 120)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(120, 84)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(84, num_classes)
+
+    def forward(self, x):
+        out = self.c1(x)
+        out = self.c2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.relu(out)
+        out = self.fc1(out)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        return out
+
+
+model = LeNet5(num_classes).to(device)
+cost = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+total_steps = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (imgs, labels) in enumerate(train_loader):
+        imgs = imgs.to(device)
+        labels = labels.to(device)
+
+        outputs = model(imgs)
+        loss = cost(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 250 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_steps, loss.item()))
+
+
+print("Training Complete!")
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
